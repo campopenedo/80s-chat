07daea82f47555eb69b5b6d783ab57d501e0f8c6
Ezequiel M. Campo Penedo <campopenedo@protonmail.com>
2021-07-27 20:21:29 +0200

    Ficheros de instalación

---
+++ instalation/create-database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE $nombreBBDD;
+
+CREATE TABLE $nombreBBDD.`ciberchat` ( `numeroMensaje` INT(11) NOT NULL AUTO_INCREMENT ,
+  `usuario` VARCHAR(30) NOT NULL DEFAULT 'Desconocid@' ,
+  `mensaje` VARCHAR(300) NOT NULL , `hora` VARCHAR(30) NOT NULL ,
+  PRIMARY KEY (`numeroMensaje`));
+
+CREATE TABLE $nombreBBDD.`usuariosConectados` ( `usuario` VARCHAR(30) NOT NULL );
---
+++ instalation/cred.php
@@ -0,0 +1,10 @@
+<?php
+/*Where the database is, like www.example.com, localhost etc.*/
+$host = '';
+/*Name of the user of the database*/
+$nombreUsuario = '';
+/*Password*/
+$contrasenhaUsuario = '';
+/*Name of the database. ciberchat by default-Don't modify-*/
+$nombreBBDD = 'ciberchat';
+?>
