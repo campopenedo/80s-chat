e203a6ae2012c724e196f94b4c9ea19866afffe3
Ezequiel Campo <campopenedo@protonmail.com>
2021-07-27 19:58:32 +0200

    Delete 80chat directory

--- 80chat/instalation/database.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE ciberChat;
-CREATE TABLE ciberChat.`ciberchat` ( `numeroMensaje` INT(11) NOT NULL AUTO_INCREMENT ,
-  `usuario` VARCHAR(30) NOT NULL DEFAULT 'Unknow' ,
-  `mensaje` VARCHAR(300) NOT NULL , `hora` VARCHAR(30) NOT NULL ,
-  PRIMARY KEY (`numeroMensaje`));
-  CREATE TABLE $nombreBBDD.`usuariosConectados` ( `usuario` VARCHAR(30) NOT NULL );
