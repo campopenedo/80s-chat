5d10d41ab43e5785e853dda92c43eae8a34a49fc
Ezequiel Campo <campopenedo@protonmail.com>
2021-07-27 20:32:35 +0200

    Update create-database.sql

--- instalation/create-database.sql
+++ instalation/create-database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE ciberchat;
 
-CREATE TABLE $nombreBBDD.`ciberchat` ( `numeroMensaje` INT(11) NOT NULL AUTO_INCREMENT ,
+CREATE TABLE ciberchat.`ciberchat` ( `numeroMensaje` INT(11) NOT NULL AUTO_INCREMENT ,
   `usuario` VARCHAR(30) NOT NULL DEFAULT 'Desconocid@' ,
   `mensaje` VARCHAR(300) NOT NULL , `hora` VARCHAR(30) NOT NULL ,
   PRIMARY KEY (`numeroMensaje`));
 
-CREATE TABLE $nombreBBDD.`usuariosConectados` ( `usuario` VARCHAR(30) NOT NULL );
+CREATE TABLE ciberchat.`usuariosConectados` ( `usuario` VARCHAR(30) NOT NULL );
